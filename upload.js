45bbaa88002c1ae4210ceb6b61b8c353efb0af8d
Alvin <alvinonline@pm.me>
2022-08-07 11:18:51 +0100

    It's modern now ✨

--- upload.js
+++ upload.js
@@ -1,9 +1,11 @@
-const fs = require('fs').promises;
-const fetch = require('node-fetch');
-const glob = require('glob-promise');
-const path = require('path');
+import 'dotenv/config';
+import { basename } from 'path';
+import fs from 'node:fs/promises';
+import fetch from 'node-fetch';
+import glob from 'glob-promise';
 
 const start = async () => {
+  const { TOKEN } = process.env;
   const files = await glob('temp/*.*');
 
   const file = files[0];
@@ -17,50 +19,27 @@ const start = async () => {
     headers: {
       Authorization: `Bearer ${TOKEN}`,
       'Dropbox-API-Arg': JSON.stringify({
-        path: `/${path.basename(file)}`,
+        path: `/${basename(file)}`,
         mode: 'overwrite',
       }),
       'Content-Type': 'application/octet-stream',
     },
   });
 
-  console.log(res.status, res.statusText);
+  if (res.ok) {
+    console.log(res.status, res.statusText);
+  }
+
+  // it failed
+  const err = await res.text();
+  throw new Error(err);
 };
 
 start()
   .then(() => {
-    console.log('done!');
+    console.log('File uploaded!');
   })
+
   .catch((err) => {
     console.log(err);
   });
-
-// import { parse } from 'https://deno.land/std@0.79.0/flags/mod.ts';
-// import { Buffer } from 'https://deno.land/std@0.79.0/node/buffer.ts';
-
-// const args = parse(Deno.args);
-
-// const { file } = args;
-
-// const fileArray = await Deno.readFile(file);
-
-// console.log(fileArray);
-
-// console.log(Buffer.from(fileArray));
-
-// const res = await fetch('https://content.dropboxapi.com/2/files/upload', {
-//   method: 'post',
-//   body: Buffer.from(fileArray),
-//   headers: {
-//     Authorization: `Bearer ${TOKEN}`,
-//     'Dropbox-API-Arg': JSON.stringify({
-//       path: `/${file}`,
-//       mode: 'overwrite',
-//       autorename: true,
-//       mute: false,
-//       strict_conflict: false,
-//     }),
-//     'Content-Type': 'application/octet-stream',
-//   },
-// });
-// console.log(res);
